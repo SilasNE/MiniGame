5d9853c55daac301f4d09cba29ff0ebd7e22ce66
Silas Engel <silas.engel06@tutanota.com>
2026-05-05 00:13:32 +0200

    Add files via upload

---
+++ marioparty/src/main/java/com/example/marioparty/model/Dice.java
@@ -0,0 +1,17 @@
+package com.example.marioparty.model;
+
+import java.util.Random;
+
+/**
+ * Würfel 1..6. Statisch, weil zustandslos.
+ */
+public final class Dice {
+
+    private static final Random rng = new Random();
+
+    private Dice() {}
+
+    public static int roll() {
+        return rng.nextInt(6) + 1;
+    }
+}
