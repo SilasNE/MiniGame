6a8e18207746ac9d1b4efd66c9603726d0d5af52
Silas Engel <silas.engel06@tutanota.com>
2026-05-05 00:14:21 +0200

    Delete Java_MiniGame directory

--- Java_MiniGame/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.java_minigame {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.java_minigame to javafx.fxml;
-    exports com.example.java_minigame;
-}
