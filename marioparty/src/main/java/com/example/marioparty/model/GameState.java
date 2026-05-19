a11892d06af3ae6c4781a157faa9825d71968c44
Silas Engel <silas.engel06@tutanota.com>
2026-05-19 19:34:21 +0200

    unnötige initialisierung entfernt

--- marioparty/src/main/java/com/example/marioparty/model/GameState.java
+++ marioparty/src/main/java/com/example/marioparty/model/GameState.java
@@ -16,7 +16,6 @@ public class GameState {
 
     public GameState() {
         board = new Board();
-        restartMatch(2, 5);
     }
 
     public void restartMatch(int humanPlayerCount, int starsGoal) {
